874fe28a2de7d5002bd93c708545dc4842f3e009
Jevona Magdalena <jmagdalena@ictual.com>
2023-04-20 16:49:51 +0200

    routing

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+let username;
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
+router.post('/', (req,res) => {
+    if(req.body.name){
+        username = req.body.name
+    }
+    res.render('trivia',
+    {
+        name: username
+    });
+});
 
 module.exports = router;
