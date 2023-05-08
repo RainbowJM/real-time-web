7329430b75d72b03a3f06d9cf0b1086e1efd2977
Jevona Magdalena <jmagdalena@ictual.com>
2023-05-08 13:42:10 +0200

    update

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io')(http);
-const port = process.env.PORT || 8080
-const bodyParser = require('body-parser')
+const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+const { createClient } = require('@supabase/supabase-js');
 // const entities = require("entities")
+const supabase = createClient(
+    'https://fgjcfncjxisqiskmnpes.supabase.co',
+    'fgjcfncjxisqiskmnpes'
+  );
 
 app.use(express.static(path.resolve('public')));
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
